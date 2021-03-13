822a14897ceb43be3b608c52b5fd256ec9920760
Charles <cvasquez39@gmail.com>
2021-03-13 12:00:33 -0500

    Update RemoveADUsersV2.ps1

--- RemoveADUsersV2.ps1
+++ RemoveADUsersV2.ps1
@@ -1,33 +1,31 @@
-﻿# Import the AD Module
+<#
+    .SYNOPSIS
+        Deactivate Active directory Users. Move Deactivated AD users to Disabled Users Directory
+    .DESCRIPTION
+        This script searches the AD for a users or list of users. To Search a list
+        Bulk users: Import a comma deliminated file
+        
+#>
+
+# Import the AD Module
 Import-Module -Name ActiveDirectory 
 
-# Function to Search for Disabled users in all OUs exculding the disabled folder
+# Function to Searches the Active Directory for disabled users in all OUs exculding the your disabled OU
 function ADSearch {
-
-#Search the AD for disabled user accounts
-Search-ADAccount -AccountDisabled | Where {$_.DistinguishedName -notlike “*OU=Disabled_Users,OU=Sites,DC=arcmon,DC=local*”}
-
+  #Search the AD for disabled user accounts
+  Search-ADAccount -AccountDisabled | Where {$_.DistinguishedName -notlike “*OU=Disabled_Users,OU=Sites,DC=arcmon,DC=local*”}| Select-Object Name, DistinguishedName | export-csv c:\temp\$((Get-Date).ToString('MM-dd-yyyy_hh-mm-ss'))_SearchADforDelUsrs.csv
 }
+# List all accounts which are already disabled on your AD and save report
+ADSearch 
 
-# Import Comma seperated file and assign it to a variable
+# Import list of users
 $csv = Import-csv -Path "c:\temp\O365Users.csv"
 
-# Disable the users based on the imported comma seperated file
+# Disable User Account
 $csv | ForEach-Object {(set-AdUser -Identity $_.samaccount -Enable $false)}
 
-# Check if users in list were disabled
+# Report of disabled users
 $csv | ForEach-Object {(get-aduser  -Identity $_.samaccount -filter {Enabled -eq $false} | select samAcGivenName)}
 
-
-# List all accounts which are already disabled on your AD
-#Search-ADAccount -AccountDisabled -SearchBase "OU=Sites,DC=arcmon,DC=local" | Select-Object Name, DistinguishedName | export-csv c:\temp\ADdisabledUsers
-ADSearch
-#Seach the AD for Deletd account
-ADSearch | Select-Object Name, DistinguishedName | export-csv c:\temp\$((Get-Date).ToString('MM-dd-yyyy_hh-mm-ss'))_SearchADforDelUsr.csv
-
 # Move all disabled AD users from others OU to the disabled users OU
-# $DisabledUsers = Search-ADAccount -AccountDisabled | Where {$_.DistinguishedName -notlike “*OU=Disabled_Users,OU=Sites,DC=arcmon,DC=local*”}
-# Write-output $DisabledUsers | Export-CSV -Path c:\temp\$((Get-Date).ToString('MM-dd-yyyy_hh-mm-ss'))_ADdisabledUsers.csv
 ADSearch | Move-ADObject -TargetPath “OU=Disabled_Users,OU=Sites,DC=arcmon,DC=local” | Export-CSV -Path c:\temp\$((Get-Date).ToString('MM-dd-yyyy_hh-mm-ss'))_MovADdisabledUsr.csv
-
-
